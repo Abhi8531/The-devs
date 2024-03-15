3de05bb50365b15cd946f1b322a4df5d564d8fb6
Abhishek Kumar <abhishekvnagar85@gmail.com>
2024-03-15 17:41:36 +0530

    Push

---
+++ src/Products.js
@@ -0,0 +1,108 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Hoodie",
+    price: 2000,
+    imageUrl:
+      "./images/hoodie.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Phone",
+    price: 3000,
+    imageUrl: "./images/Phone.png",
+  },
+
+  {
+    id: 3,
+    title: "Shoes",
+    price: 3050,
+    imageUrl: "./images/Shoes.avif",
+  },
+
+  {
+    id: 4,
+    title: "Sunglasses",
+    price: 950,
+    imageUrl: "./images/sun.jpg",
+  },
+
+  {
+    id: 5,
+    title: "Football",
+    price: 3500,
+    imageUrl:
+      "./images/foot.jpg",
+  },
+
+  {
+    id: 6,
+    title: "Sandles",
+    price: 1200,
+    imageUrl: "./images/san.jpg",
+  },
+
+  {
+    id: 7,
+    title: "Bed",
+    price: 5000,
+    imageUrl: "./images/500$_flaxby-sprung-base.jpg",
+  },
+
+  {
+    id: 8,
+    title: "Watch",
+    price: 5500,
+    imageUrl: "./images/Watch.jpg",
+  },
+
+  {
+    id: 9,
+    title: "Cap",
+    price: 1290,
+    imageUrl: "./images/cap.jpg",
+  },
+
+  {
+    id: 10,
+    title: "Cricket Bat",
+    price: 6500,
+    imageUrl: "./images/bat.jpg",
+  },
+
+  {
+    id: 11,
+    title: "T-Shirt",
+    price: 7090,
+    imageUrl: "./images/t.jpg",
+  },
+
+  {
+    id: 12,
+    title: "Shirt",
+    price: 1190,
+    imageUrl: "./images/shirt.jpg",
+  },
+
+  {
+    id: 13,
+    title: "BED",
+    price: 4000,
+    imageUrl: "./images/400$_sleepeezee-drawer-divan-base.jpg",
+  },
+
+  {
+    id: 14,
+    title: "BED",
+    price: 5000,
+    imageUrl: "./images/500$_therapur-shallow-divan-base.jpg",
+  },
+
+  {
+    id: 15,
+    title: "BED",
+    price: 3500,
+    imageUrl: "./images/350$_classic-ottoman-base.jpg",
+  },
+];
